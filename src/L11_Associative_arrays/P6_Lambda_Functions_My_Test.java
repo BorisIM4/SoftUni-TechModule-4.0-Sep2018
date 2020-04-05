d1bb4dd56d745b0d1c33a4b6eb351546b896851b
JRB <jrb@abv.bg>
2020-04-05 17:55:30 +0300

    This is first commit from this project.

--- src/L11_Associative_arrays/P6_Lambda_Functions_My_Test.java
+++ src/L11_Associative_arrays/P6_Lambda_Functions_My_Test.java
@@ -6,5 +6,6 @@ public class P6_Lambda_Functions_My_Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
     }
 }
--- src/L11_Associative_arrays_exercise/P3_Legendary_Farming.java
+++ src/L11_Associative_arrays_exercise/P3_Legendary_Farming.java
@@ -1,11 +1,65 @@
 package L11_Associative_arrays_exercise;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class P3_Legendary_Farming {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        boolean isItObtained = false;
 
+        Map<String, Integer> importantPieces = new HashMap<>();
+        importantPieces.put("shards", 0);
+        importantPieces.put("fragments", 0);
+        importantPieces.put("motes", 0);
+        Map<String, Integer> junkPieces = new TreeMap<>();
+        while (!isItObtained) {
+            String[] input = scanner
+                    .nextLine()
+                    .split("\\s+");
+
+            for (int i = 0; i < input.length - 1; i += 2) {
+                String materialType = input[i + 1].toLowerCase();
+                int materialQuantity = Integer.parseInt(input[i]);
+
+                if (materialType.equals("shards") || materialType.equals("fragments") ||
+                        materialType.equals("motes")) {
+                    importantPieces.put(materialType, importantPieces.get(materialType) + materialQuantity);
+
+                    if (importantPieces.get(materialType) >= 250) {
+                        importantPieces.put(materialType, importantPieces.get(materialType) - 250);
+                        if (materialType.equals("shards")) {
+                            System.out.println("Shadowmourne obtained!");
+                        } else if (materialType.equals("fragments")) {
+                            System.out.println("Valanyr obtained!");
+                        } else {
+                            System.out.println("Dragonwrath obtained!");
+                        }
+                        isItObtained = true;
+                        break;
+                    }
+
+                } else {
+                    if (!junkPieces.containsKey(materialType)) {
+                        junkPieces.put(materialType, materialQuantity);
+                    } else {
+                        junkPieces.put(materialType, junkPieces.get(materialType) + materialQuantity);
+                    }
+                }
+            }
+        }
+
+        importantPieces.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue()
+                        .reversed()
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .forEach(entry -> {
+                    System.out.println(String.format("%s: %d", entry.getKey(), entry.getValue()));
+                });
+
+        junkPieces.entrySet().stream().forEach(entry -> {
+            System.out.println(String.format("%s: %d", entry.getKey(), entry.getValue()));
+        });
     }
 }
